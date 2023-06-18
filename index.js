ac899a747c1cd76dd337139bf78f14d7ac0d3357
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-18 17:30:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(action = "roller-skate"){
+     return `This Saturday, I want to ${action}!`;
+
+
+     
+
+     
+ }
+  function mondayWork(work ="go to the office"){
+    return `This Monday, I will ${work}.`;
+
+  } 
+  //
+  function wrapAdjective(wrap = "*"){
+
+    function  write (busy){
+        return `You are ${wrap}${busy}${wrap}!`;
+
+    }
+    return write  ;
+
+    
+
+  }
+
+
